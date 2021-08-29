40d46ed6442ac34f342a7be35e64380e922d6885
kritika243 <24kritikasharma24@gmail.com>
2021-08-29 21:09:58 +0530

    :dizzy: animation, scrollReveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,3 +68,22 @@ function scrollActive() {
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset:true
+})
+
+sr.reveal(`.home_header, .section_title`, { delay: 600 })
+sr.reveal(`.home_footer`, { delay: 700 })
+sr.reveal(`.home_img`, { delay: 900, origin: 'top' })
+
+sr.reveal(
+  `.sponsor_img, .products_card, .footer_logo, .footer_content, .footer_copy`,
+  { origin: 'top', interval: 100 }
+)
+sr.reveal(`.specs_data, .discount_animate`, { origin: 'left', interval: 100 })
+sr.reveal(`.specs_img, .discount_img`, { origin: 'right' })
+sr.reveal(`.case_img`, { origin: 'top' })
+sr.reveal(`.case_data`)
