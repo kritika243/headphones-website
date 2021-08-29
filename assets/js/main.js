c21d0d2e2931abd3fae67ab942d733798b129ac5
kritika243 <24kritikasharma24@gmail.com>
2021-08-29 20:26:12 +0530

    scrolling up and making links active

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,35 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up')
+  if (this.scrollY >= 200) scrollUp.classList.add('show-scroll')
+  else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.add('active-link')
+    } else {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
